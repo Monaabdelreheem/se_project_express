e5c3ff398fe0e530be3fbb286595d0d3291b5985
Mona Abdelreheem <m.abdelraheem@aol.com>
2026-01-10 15:55:54 -0600

    Fix login error handling and use status code constants

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,7 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
-const { BAD_REQUEST, NOT_FOUND, SERVER_ERROR, UNAUTHORIZED } = require("../utils/errors");
+const { BAD_REQUEST, NOT_FOUND, SERVER_ERROR, UNAUTHORIZED, CONFLICT } = require("../utils/errors");
 const { JWT_SECRET } = require("../utils/config");
 
 
@@ -91,9 +91,14 @@ const login = async (req, res) => {
     const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: "7d" });
     return res.status(200).send({ token });
   } catch (err) {
+    if (err.message === "Invalid email or password") {
+      return res
+        .status(UNAUTHORIZED)
+        .send({ message: "Invalid email or password" });
+    }
     return res
-      .status(UNAUTHORIZED)
-      .send({ message: "Invalid email or password" });
+      .status(SERVER_ERROR)
+      .send({ message: "An error has occurred on the server." });
   }
 };
 
