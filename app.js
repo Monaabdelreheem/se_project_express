7593c4cab79d4029346e2bdd69eb25e07a4c7d2f
Mona Abdelreheem <m.abdelraheem@aol.com>
2026-01-11 15:49:59 -0600

    Handle MongoDB connection errors

--- app.js
+++ app.js
@@ -8,7 +8,15 @@ const {createUser, login} = require("./controllers/users");
 const app = express();
 const { PORT = 3001 } = process.env;
 
-mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
+
 
 app.use(express.json());
 app.use(cors());
@@ -18,5 +26,7 @@ app.post("/signin", login);
 
 app.use("/", mainRouter);
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 module.exports = app;
