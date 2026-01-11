3d731424832b04962446a4c4dc8fc6212dfd88f6
Mona Abdelreheem <m.abdelraheem@aol.com>
2026-01-11 16:11:51 -0600

    Fix public signup, signin, and items routes and protect others

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,11 @@ const auth = require("../middlewares/auth");
 const { NOT_FOUND } = require("../utils/errors");
 const clothingItemsRouter = require("./clothingItems");
 const { getItems } = require("../controllers/clothingItems");
+const { createUser, login } = require("../controllers/users");
 
-// public route
+// public routes
+router.post("/signup", createUser);
+router.post("/signin", login);
 router.get("/items", getItems);
 
 
