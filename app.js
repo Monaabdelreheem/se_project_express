d69918a4526becf5f21ede5e820c0db447fbe927
Mona Abdelreheem <m.abdelraheem@aol.com>
2026-01-26 23:07:55 -0600

    Fix delete button logic and update dependencies

--- app.js
+++ app.js
@@ -3,19 +3,18 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const mainRouter = require("./routes/index");
 
-
 const app = express();
 const { PORT = 3001 } = process.env;
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .connect("mongodb://localhost:27017/wtwr_db")
   .then(() => {
     console.log("Connected to MongoDB");
+
   })
   .catch((err) => {
     console.error("MongoDB connection error:", err);
   });
-
 
 app.use(express.json());
 app.use(cors());
