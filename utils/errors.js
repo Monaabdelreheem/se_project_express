574180e4383d917671ecb7f6960779f0fa61cc0e
Mona Abdelreheem <m.abdelraheem@aol.com>
2026-02-06 00:31:00 -0600

    Add custom error classes

--- utils/errors.js
+++ utils/errors.js
@@ -5,6 +5,40 @@ const NOT_FOUND = 404;
 const CONFLICT = 409;
 const SERVER_ERROR = 500;
 
+class BadRequestError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = BAD_REQUEST;
+  }
+}
+
+class UnauthorizedError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = UNAUTHORIZED;
+  }
+}
+
+class ForbiddenError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = FORBIDDEN;
+  }
+}
+
+class NotFoundError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = NOT_FOUND;
+  }
+}
+
+class ConflictError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = CONFLICT;
+  }
+}
 
 module.exports = {
   BAD_REQUEST,
@@ -13,4 +47,9 @@ module.exports = {
   SERVER_ERROR,
   UNAUTHORIZED,
   CONFLICT,
+  BadRequestError,
+  UnauthorizedError,
+  ForbiddenError,
+  NotFoundError,
+  ConflictError,
 };
