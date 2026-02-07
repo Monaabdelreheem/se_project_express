afc5d31f038a53be3b7f0d50dba7f28343f8d58a
Mona Abdelreheem <m.abdelraheem@aol.com>
2026-02-07 04:33:00 -0600

    Add celebrate error handler

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const { errors } = require("celebrate");
 const mainRouter = require("./routes/index");
 const errorHandler = require("./middlewares/error-handler");
 
@@ -22,6 +23,7 @@ app.use(cors());
 
 app.use("/", mainRouter);
 
+app.use(errors());
 app.use(errorHandler);
 
 app.listen(PORT, () => {
